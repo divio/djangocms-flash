f55ca965dc7a54958bb209bd3c50efcf4cc40fe5
Iacopo Spalletti <i.spalletti@nephila.it>
2014-08-16 14:06:53 +0200

    Recreate migrations

--- djangocms_flash/migrations_django/0001_initial.py
+++ djangocms_flash/migrations_django/0001_initial.py
@@ -8,17 +8,17 @@ import cms.models.pluginmodel
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '__first__'),
+        ('cms', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Flash',
             fields=[
+                ('cmsplugin_ptr', models.OneToOneField(serialize=False, parent_link=True, auto_created=True, to='cms.CMSPlugin', primary_key=True)),
                 ('file', models.FileField(help_text='use swf file', upload_to=cms.models.pluginmodel.get_plugin_media_path, verbose_name='file')),
-                ('width', models.CharField(max_length=6, verbose_name='width')),
-                ('height', models.CharField(max_length=6, verbose_name='height')),
-                ('cmsplugin_ptr', models.OneToOneField(auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('width', models.CharField(verbose_name='width', max_length=6)),
+                ('height', models.CharField(verbose_name='height', max_length=6)),
             ],
             options={
                 'abstract': False,
