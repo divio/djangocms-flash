fd47e3d7b7abc74eabb1e349b4355c45651e604a
Martin Koistinen <mkoistinen@gmail.com>
2015-12-02 16:57:10 -0500

    The migration shuffle

--- djangocms_flash/migrations/0001_initial.py
+++ djangocms_flash/migrations/0001_initial.py
@@ -1,62 +1,28 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models, connection
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import cms.models.pluginmodel
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        table_names = connection.introspection.table_names()
-        if 'cmsplugin_flash' in table_names:
-            db.rename_table('cmsplugin_flash', 'djangocms_flash_flash')
-        elif 'flash_flash' in table_names:
-            db.rename_table('flash_flash', 'djangocms_flash_flash')
-        else:
-            # Adding model 'Flash'
-            db.create_table(u'djangocms_flash_flash', (
-                (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-                ('file', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
-                ('width', self.gf('django.db.models.fields.CharField')(max_length=6)),
-                ('height', self.gf('django.db.models.fields.CharField')(max_length=6)),
-            ))
-            db.send_create_signal(u'djangocms_flash', ['Flash'])
+    dependencies = [
+        ('cms', '__first__'),
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'Flash'
-        db.delete_table(u'djangocms_flash_flash')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        u'djangocms_flash.flash': {
-            'Meta': {'object_name': 'Flash', '_ormbases': ['cms.CMSPlugin']},
-            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
-            'height': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
-            'width': ('django.db.models.fields.CharField', [], {'max_length': '6'})
-        }
-    }
-
-    complete_apps = ['djangocms_flash']
+    operations = [
+        migrations.CreateModel(
+            name='Flash',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(serialize=False, parent_link=True, auto_created=True, to='cms.CMSPlugin', primary_key=True)),
+                ('file', models.FileField(help_text='use swf file', upload_to=cms.models.pluginmodel.get_plugin_media_path, verbose_name='file')),
+                ('width', models.CharField(verbose_name='width', max_length=6)),
+                ('height', models.CharField(verbose_name='height', max_length=6)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
